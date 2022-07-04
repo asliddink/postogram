8b2c7c83dbc733b0553ebf53a36221966cc77410
asliddink <asliddinkarimboyev@gmail.com>
2022-07-04 18:29:30 +0500

    todey is yes of course

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,12 +1,58 @@
-import { Router } from 'express'
+import { Router } from 'express';
+import { createUser, findUser } from '../service/user.service.js';
+import md5 from 'md5';
+
 const router = Router()
 
-router.post('/login', (req,res) =>{
-    res.send('Login')
+router.post('/register', async (req,res) =>{
+    const { username, password, firstName, lastName, age } = req.body
+
+    const existUser = await findUser(username)
+
+    if(existUser){
+        res.status(400).json({
+            masssage: `User with username ${username} already exist.`
+        })
+    }
+    else{
+        const token = md5(username + ':' + password)
+        const newUser = await createUser(username, password, firstName, lastName, age,token)
+
+        res.status(201).json({
+            masssage: 'User created.',
+            user:{
+                username,
+                token
+            }
+        })
+    }
 })
 
-router.post('/register', (req,res) =>{
-    res.send('Register')
+router.post('/login', async (req,res) =>{
+    const { username,password } = req.body
+
+    const exist = await findUser(username)
+    if (!existUser){
+        res.status(400).json({
+            masssage: `User with username ${username} not found`
+        })
+    }
+    else if(existUser.password !== password){
+    res.status(400).json({
+        masssage:'Wrong username or password.'
+    })
+    else{
+        res.status(201).json({
+            masssage: 'Succsesful logged id.',
+            user: {
+                username: existUser.username,
+                token: existUser.token
+            }
+        })
+    }
+}
+
+
 })
 
 export default router
